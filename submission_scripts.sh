e19e3c8c8fd8d26b78b6fab6490f4ff9b3a4df58
Elodie Paupe <elodie.paupe@ik.me>
2022-03-06 16:33:20 +0100

    add script

---
+++ submission_scripts.sh
@@ -0,0 +1,18 @@
+
+#!/bin/env bash
+#SBATCH --partition=public-gpu
+#SBATCH --time=2-00:00:00
+#SBATCH --gpus=1
+#SBATCH --output=kraken-%j.out
+#SBATCH --mem=0
+
+module load fosscuda/2020b Python/3.8.6
+source ~/kraken-env/bin/activate
+pip install albumentations
+
+
+OUTPUT_NAME="output_name"
+XML_FOLDER="/home/users/p/paupeel1/corpus_prevot_farine_fr"
+
+echo "KETOS training"
+srun ketos segtrain -f alto -t train.txt -e eval.txt -d cuda "${XML_FOLDER}/*.xml"
