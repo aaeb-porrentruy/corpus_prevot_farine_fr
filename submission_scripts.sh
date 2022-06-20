0915b95db5beb57297630779f222df9eda95ad2b
elodiepaupe <42827908+elodiepaupe@users.noreply.github.com>
2022-06-20 13:32:01 +0200

    scritp

--- submission_scripts.sh
+++ submission_scripts.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
-#SBATCH --partition=public-gpu
-#SBATCH --time=2-00:00:00
+#SBATCH --partition=shared-gpu
+#SBATCH --time=02:00:00
 #SBATCH --gpus=1
 #SBATCH --output=kraken-%j.out
 #SBATCH --mem=0
@@ -9,7 +9,7 @@ module load fosscuda/2020b Python/3.8.6
 source ~/kraken-env/bin/activate
 
 OUTPUT_NAME="output_name"
-XML_FOLDER="/home/users/p/paupeel1/corpus_prevot_farine_fr/B168"
+XML_FOLDER="/home/users/g/paupeel1/corpus_prevot_farine_fr/B168"
 
 echo "KETOS training"
 srun ketos train -o $OUTPUT_NAME -f alto -t train.txt -e eval.txt -d cuda "${XML_FOLDER}/*.xml"
